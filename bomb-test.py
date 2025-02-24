2f3e68474afc4818295414272c1a22e2f5e3cf14
megandowdell <megan@dowdellfamily.com>
2025-02-24 14:42:09 -0500

    Create bomb-test.py

---
+++ bomb-test.py
@@ -0,0 +1,14 @@
+# Python code testing file for Bomb components (LED, switches, button, keypad, wires, timer)
+
+
+# Testing LEDs
+
+# Testing Switches
+
+# Testing Button
+
+# Testing Keypad
+
+# Testing Wires
+
+# Testing Timer
