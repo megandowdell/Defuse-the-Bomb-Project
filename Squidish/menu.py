cf93b9092862f1cf9dc8ece1fc9e42902d54ab97
megandowdell <megan@dowdellfamily.com>
2025-05-02 18:44:49 -0400

    Update menu.py

--- Squidish/menu.py
+++ Squidish/menu.py
@@ -1163,6 +1163,7 @@ def show_hopscotch_game_screen(screen):
                         else:
                             lives -= 1
                             current_row = 0
+                            tile_rect = get_tile_rect(row, col, current_row)
                             pygame.draw.rect(screen, FAIL, tile_rect)
                             print("WRONG TILE — Strike!")
                             if lives == 0:
