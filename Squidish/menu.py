7eefdb27a5aa6004a9403df3b4c388438fb3ca6e
megandowdell <megan@dowdellfamily.com>
2025-05-02 17:30:21 -0400

    Update menu.py

--- Squidish/menu.py
+++ Squidish/menu.py
@@ -1151,7 +1151,7 @@ def show_hopscotch_game_screen(screen):
                 if True:
                     # Get the index of the flipped toggle (0–3)
                     selected_col = next(i for i, (a, b) in enumerate(zip(toggles._value, toggles._prev_value)) if a != b)
-                    # print(selected_col)
+                    print(selected_col)
         
                     if selected_col is not None:
                         # Check if selected toggle is correct for current row
