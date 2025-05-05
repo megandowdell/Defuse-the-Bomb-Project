6dfb433c408567c33e021609f810c6901bd197ab
christamaries <114307809+christamaries@users.noreply.github.com>
2025-05-05 12:03:17 -0700

    Update menu.py

--- Squidish/menu.py
+++ Squidish/menu.py
@@ -406,8 +406,8 @@ def show_about_game_screen(screen):
                     if ag_items[selected_index] == "Back": # Back button returns to menu
                         return "Menu"  
                     elif ag_items[selected_index] == "Continue": # Continue button proceeds to game just as Start button would on the menu page
-                        random.choice(["Hopscotch", "Tic Tac Toe"])
-                        return random.choice(["Hopscotch", "Tic Tac Toe"]) 
+                        random.choice(["Hopscotch", "Tic Tac Toe", "Red Light Green Light"])
+                        return random.choice(["Hopscotch", "Tic Tac Toe", "Red Light Green Light"]) 
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
                 for rect, item in button_rects:
@@ -415,7 +415,7 @@ def show_about_game_screen(screen):
                         if item == "Back":
                             return "Menu"  
                         elif item == "Continue":
-                            return random.choice(["Hopscotch", "Tic Tac Toe"])  
+                            return random.choice(["Hopscotch", "Tic Tac Toe", "Red Light Green Light"])  
         pygame.display.flip()
         clock.tick(60)
 ####################################################################################################################
@@ -2291,7 +2291,7 @@ def main():
     
     game_running = True
     game_state = "Menu"
-    mini_games = ["Hopscotch", "Tic Tac Toe"]
+    mini_games = ["Hopscotch", "Tic Tac Toe", "Red Light Green Light"]
     completed_games = set()
 
     while game_running:
