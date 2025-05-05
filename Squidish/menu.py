e9696533990fe73ec9ee620ed27361c6a0ea5648
christamaries <114307809+christamaries@users.noreply.github.com>
2025-05-04 19:51:36 -0700

    Update menu.py

--- Squidish/menu.py
+++ Squidish/menu.py
@@ -428,7 +428,7 @@ def show_meet_team(screen):
     # Base font sizes
     base_title_size = 50
     base_char_size = 30
-    base_desc_size = 14
+    base_desc_size = 12
     base_button_size = 20
     
     # Scale font sizes
