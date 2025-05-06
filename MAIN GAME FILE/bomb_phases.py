704b89172ae1c4a26c0abdc6ac8941a29bfa146f
christamaries <114307809+christamaries@users.noreply.github.com>
2025-05-06 17:52:13 -0400

    Update bomb_phases.py

--- MAIN GAME FILE/bomb_phases.py
+++ MAIN GAME FILE/bomb_phases.py
@@ -155,25 +155,41 @@ class Timer(PhaseThread):
         self._interval = 1
 
     # runs the thread - adds logic to code
+    # def run(self):
+    #     self._running = True
+    #     while (self._running):
+    #         if (not self._paused):
+    #             # update the timer and display its value on the 7-segment display
+    #             self._update()
+    #             self._component.print(str(self))
+    #             # wait 1s (default) and continue
+    #             sleep(self._interval)
+    #             # the timer has expired -> phase failed (explode)
+    #             # self._value -= 1
+    #             # if self._value <= 0:
+    #             #     self._running = False
+
+    #             if (self._value == 0):
+    #                 self._running = False
+    #             self._value -= 1
+    #         else:
+    #             sleep(0.1)
+
     def run(self):
         self._running = True
-        while (self._running):
-            if (not self._paused):
-                # update the timer and display its value on the 7-segment display
+        while self._running:
+            if not self._paused:
                 self._update()
                 self._component.print(str(self))
-                # wait 1s (default) and continue
                 sleep(self._interval)
-                # the timer has expired -> phase failed (explode)
-                # self._value -= 1
-                # if self._value <= 0:
-                #     self._running = False
-
-                if (self._value == 0):
+                
+                self._value -= 1  # First decrement
+    
+                if self._value < 0:
                     self._running = False
-                self._value -= 1
             else:
                 sleep(0.1)
+
 
     # updates the timer (only internally called)
     def _update(self):
