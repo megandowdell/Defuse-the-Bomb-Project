2e56571600a01ea99c134aea88908795d39dbb18
christamaries <114307809+christamaries@users.noreply.github.com>
2025-05-06 14:40:03 -0400

    Update MENU.py

--- MAIN GAME FILE/MENU.py
+++ MAIN GAME FILE/MENU.py
@@ -3149,7 +3149,7 @@ def main():
     while game_running:
         if timer._value == 0 and game_state not in ["Menu", "About Game", "Meet Team", "Win", "Death"]:
             game_state = "Death"  # Time's up, show death screen
-            continue
+            pygame.quit()
         if game_state == "Menu":
             completed_games.clear()  # Reset progress when returning to menu
             pygame.mixer.music.load("pink_soldiers.mp3")
