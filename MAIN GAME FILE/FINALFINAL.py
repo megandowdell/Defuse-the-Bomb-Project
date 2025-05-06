e2f2a5cf55209297210c97d3f769e3f7f4302964
christamaries <114307809+christamaries@users.noreply.github.com>
2025-05-06 01:55:37 -0400

    Update FINALFINAL.py

--- MAIN GAME FILE/FINALFINAL.py
+++ MAIN GAME FILE/FINALFINAL.py
@@ -3063,11 +3063,6 @@ def main():
                 pygame.mixer.music.play(-1)
                 result = show_simon_says_game_screen(screen)  
            
-            if timer.has_expired():
-            # Show game over screen and exit
-                show_death_screen(screen)
-                game_running = False
-                continue
             
             # Handle result
             if result == "win":
